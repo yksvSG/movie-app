442c638dbf87a3498f0b7f7770476e9a92281150
yksvSG <sg.yksv77@gmail.com>
2023-02-24 11:25:55 +0900

    ts 전환 전

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import TheFooter from "./components/TheFooter";
 export default class App extends Component {
   render() {
     const theHeader = new TheHeader().el;
-    const routerView = document.createElement("router-view");
     const theFooter = new TheFooter().el;
+    const routerView = document.createElement("router-view");
+
     this.el.append(theHeader, routerView, theFooter);
   }
 }
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import App from "./App";
+import router from "./routes";
+
+const root = document.querySelector("#root");
+// 타입 가드
+// if (root) {
+//   root.append(new App().el);
+// }
+root?.append(new App().el);
+// 루트 요소를 등록한 후에 실행해야 하는 플러그인!
+router();
