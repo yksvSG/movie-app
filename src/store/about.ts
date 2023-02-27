023d86a88f2fa3eef4203a2883d507e14f3c917f
yksvSG <sg.yksv77@gmail.com>
2023-02-27 18:57:04 +0900

    About 스토어 타이핑 완료/ JSON to TS

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,15 @@
 import { Store } from "../core/common";
 
-export default new Store({
+interface State {
+  photo: string;
+  name: string;
+  email: string;
+  blog: string;
+  github: string;
+  repository: string;
+}
+
+export default new Store<State>({
   photo:
     "https://avatars.githubusercontent.com/u/93868114?s=400&u=dfa313a944bb35561a1504bd58e602eeaa193f72&v=4",
   name: "Guny / BaeSangGun",
